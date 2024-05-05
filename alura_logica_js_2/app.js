f97273e47ea37b088823c388f803b841cf9282f1
TheUndeadGod <48629754+TheUndeadGod@users.noreply.github.com>
2024-05-05 17:31:03 -0300

    Update app.js
    teste remoção de template String para teste de deploy no versel

--- alura_logica_js_2/app.js
+++ alura_logica_js_2/app.js
@@ -8,8 +8,8 @@ function reiniciarJogo() {
     numeroSecreto = gerarNumeroAleatorio();
     limparCampo();
     tentativas = 0;
-    exibirTextoNaTela('h1', 'Jogo do número secreto');
-    exibirTextoNaTela('p', `Escolha um número entre 1 e ${valorMaximoAleatorio}`);
+    //exibirTextoNaTela('h1', 'Jogo do número secreto');
+    //exibirTextoNaTela('p', `Escolha um número entre 1 e ${valorMaximoAleatorio}`);
     document.getElementById('reiniciar').setAttribute('disabled', true)
     document.getElementById('chutar').removeAttribute('disabled');
 }
@@ -30,15 +30,15 @@ function verificarChute() {
     }
     if (chute == numeroSecreto) {
         exibirTextoNaTela('h1', 'Acertou!');                
-        exibirTextoNaTela('p', `Você descobriu o número secreto com ${tentativas} ${tentativas > 1 ? 'tentativas' : 'tentativa'}!`);
+        //exibirTextoNaTela('p', `Você descobriu o número secreto com ${tentativas} ${tentativas > 1 ? 'tentativas' : 'tentativa'}!`);
         document.getElementById('chutar').setAttribute('disabled', true)
         document.getElementById('reiniciar').removeAttribute('disabled');
         limparCampo();
     } else {
         if (chute > numeroSecreto) {
-            exibirTextoNaTela('p', `O número secreto é menor que ${chute}`);
+            //exibirTextoNaTela('p', `O número secreto é menor que ${chute}`);
         } else {
-            exibirTextoNaTela('p', `O número secreto é maior que ${chute}`);
+            //exibirTextoNaTela('p', `O número secreto é maior que ${chute}`);
         }
         limparCampo();
     }
